2e1b38e90ea2a89ba5ba93b065c86cba12425941
wjdrlwn0117 <77970721+wjdrlwn0117@users.noreply.github.com>
2021-04-13 18:17:25 +0900

    Add files via upload
    데이터 저장을 위한 유저모델

---
+++ UserModel.java
@@ -0,0 +1,11 @@
+package com.example.account;
+
+public class UserModel {
+    // 사용자 기본정보
+
+    public String name;
+    public long birth;
+    public String gender;
+//    public String pushToken;
+
+}
